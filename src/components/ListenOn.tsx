a35f30382b0b2e0bd600409b244e6ae16dede26b
jjacob-dev <jjacobjurgens@gmail.com>
2023-10-14 22:42:40 +1000

    fixed files

--- src/components/ListenOn.tsx
+++ src/components/ListenOn.tsx
@@ -7,7 +7,10 @@ function ListenOn() {
           <div className="text-left">
             <p>
               Listen on SoundCloud:{"   "}
-              <a className=" text-blue-500" href="https://soundcloud.com/">
+              <a
+                className=" text-blue-500"
+                href="https://soundcloud.com/jacob-jurgens-53615531/project-idx-podcast/s-mROHMWIOfyj?si=c765a2a262ae4ac0a0b816350709cfac&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+              >
                 Episode 1
               </a>
             </p>
